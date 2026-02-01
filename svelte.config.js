32a819533bbdbbe4709f88ab981632444f919d2d
dominikschopper <github@dschopper.de>
2026-02-01 09:38:57 +0100

    chore(sec): do not show svelte version

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,7 @@ const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  discloseVersion: false,
 
   kit: {
     adapter: adapter({
